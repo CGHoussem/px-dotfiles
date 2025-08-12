79459fe56b9a7828053fb60822c549073e57be0f
Houssem Ben Mabrouk <cjhoussem@gmail.com>
2025-08-12 19:45:36 +0200

    dual monitor setup
    Signed-off-by: Houssem Ben Mabrouk <cjhoussem@gmail.com>

--- i3/.config/i3/display.sh
+++ i3/.config/i3/display.sh
@@ -4,7 +4,10 @@ INTERNAL="eDP1"
 EXTERNAL="HDMI-0"
 
 if xrandr | grep "$EXTERNAL connected"; then
-    xrandr --output "$INTERNAL" --off --output "$EXTERNAL" --auto
+    #xrandr --output "$INTERNAL" --off --output "$EXTERNAL" --auto
+    xrandr -d : --output eDP1 --auto
+    xrandr --output "$EXTERNAL" --auto --primary
+    xrandr --output "$INTERNAL" --left-of "$EXTERNAL"
 else
     xrandr --output "$EXTERNAL" --off --output "$INTERNAL" --auto
 fi
