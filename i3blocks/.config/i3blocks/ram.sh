a21aaa620b3fb7474af640d394c4e0a53cce7cb9
Houssem Ben Mabrouk <cjhoussem@gmail.com>
2025-08-13 06:53:28 +0200

    fix: ram.sh i3blocks
    Signed-off-by: Houssem Ben Mabrouk <cjhoussem@gmail.com>

--- i3blocks/.config/i3blocks/ram.sh
+++ i3blocks/.config/i3blocks/ram.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-FULL_TEXT=$(df -h / | awk 'NR==2  {print "Root: " $3 "/" $2 " (" $5 ")"}')
-USAGE=$(df / | awk 'NR==2  {gsub("%",""); print $5}')
+TOTAL_MEM=$(free -m | awk '/^Mem:/ {print $2}')
+USED_MEM=$(free -m | awk '/^Mem:/ {print $3}')
+FREE_MEM=$(free -m | awk '/^Mem:/ {print $4}')
+
+# FULL_TEXT=$(df -h / | awk 'NR==2  {print "Root: " $3 "/" $2 " (" $5 ")"}')
+# USAGE=$(df / | awk 'NR==2  {gsub("%",""); print $5}')
+
+USAGE=$(awk "BEGIN {printf \"%.2f\", $USED_MEM/$TOTAL_MEM*100}")
+FULL_TEXT="RAM: ${USED_MEM}M/${TOTAL_MEM}M ($USAGE%)"
 
 echo $FULL_TEXT
-echo "/: $USAGE"
-
-[ "$USAGE" -ge 90 ] && exit 33
-[ "$USAGE" -ge 70 ] && echo "#FF8000"
+echo "RAM: ${USAGE}%"
 
 exit 0
