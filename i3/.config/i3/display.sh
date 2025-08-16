69372154fbe551718bac8598154fbb14547624c3
Houssem Ben Mabrouk <cjhoussem@gmail.com>
2025-08-16 12:48:31 +0200

    fix: display.sh i3 script
    Signed-off-by: Houssem Ben Mabrouk <cjhoussem@gmail.com>

--- i3/.config/i3/display.sh
+++ i3/.config/i3/display.sh
@@ -5,8 +5,8 @@ EXTERNAL="HDMI-0"
 
 if xrandr | grep "$EXTERNAL connected"; then
     #xrandr --output "$INTERNAL" --off --output "$EXTERNAL" --auto
-    xrandr -d : --output eDP1 --auto
-    xrandr --output "$EXTERNAL" --auto --primary
+    xrandr -d :0 --output eDP1 --auto
+    xrandr --output "$INTERNAL" --auto --primary
     xrandr --output "$INTERNAL" --left-of "$EXTERNAL"
 else
     xrandr --output "$EXTERNAL" --off --output "$INTERNAL" --auto
