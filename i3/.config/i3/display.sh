06c7b9ad2e4c546b50839efffd46dac63afbb712
Houssem Ben Mabrouk <cjhoussem@gmail.com>
2025-08-29 21:30:05 +0200

    fix: display external internal (idk)
    Signed-off-by: Houssem Ben Mabrouk <cjhoussem@gmail.com>

--- i3/.config/i3/display.sh
+++ i3/.config/i3/display.sh
@@ -6,7 +6,7 @@ EXTERNAL="HDMI-0"
 if xrandr | grep "$EXTERNAL connected"; then
     #xrandr --output "$INTERNAL" --off --output "$EXTERNAL" --auto
     xrandr -d :0 --output eDP1 --auto
-    xrandr --output "$INTERNAL" --auto --primary
+    xrandr --output "$EXTERNAL" --auto --primary
     xrandr --output "$INTERNAL" --left-of "$EXTERNAL"
 else
     xrandr --output "$EXTERNAL" --off --output "$INTERNAL" --auto
