2f01bdb30dc8663a11e8555e45d1928dabb14b8c
Houssem Ben Mabrouk <cjhoussem@gmail.com>
2026-03-06 19:37:25 +0100

    updates
    Signed-off-by: Houssem Ben Mabrouk <cjhoussem@gmail.com>

--- i3/.config/i3/display.sh
+++ i3/.config/i3/display.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-INTERNAL="eDP1"
+INTERNAL="DP-2"
 EXTERNAL="HDMI-0"
 
 if xrandr | grep "$EXTERNAL connected"; then
     #xrandr --output "$INTERNAL" --off --output "$EXTERNAL" --auto
-    xrandr -d :0 --output eDP1 --auto
+    xrandr -d :0 --output $INTERNAL --auto
     xrandr --output "$EXTERNAL" --auto --primary
-    xrandr --output "$INTERNAL" --left-of "$EXTERNAL"
+    xrandr --output "$INTERNAL" --right-of "$EXTERNAL"
 else
     xrandr --output "$EXTERNAL" --off --output "$INTERNAL" --auto
 fi
+
+xrandr --output DP-2 --primary
