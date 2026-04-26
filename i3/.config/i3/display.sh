d60ff4b2ae7c626abfd2ae18d60c7306794ee4cb
Houssem Ben Mabrouk <cjhoussem@gmail.com>
2026-04-26 12:10:29 +0200

    chore(i3): update display script
    Signed-off-by: Houssem Ben Mabrouk <cjhoussem@gmail.com>

--- i3/.config/i3/display.sh
+++ i3/.config/i3/display.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-INTERNAL="DP-2"
-EXTERNAL="HDMI-0"
+INTERNAL="eDP1"
+EXTERNAL="HDMI-1-0"
 
 if xrandr | grep "$EXTERNAL connected"; then
     #xrandr --output "$INTERNAL" --off --output "$EXTERNAL" --auto
     xrandr -d :0 --output $INTERNAL --auto
     xrandr --output "$EXTERNAL" --auto --primary
-    xrandr --output "$INTERNAL" --right-of "$EXTERNAL"
+    xrandr --output "$INTERNAL" --left-of "$EXTERNAL"
 else
     xrandr --output "$EXTERNAL" --off --output "$INTERNAL" --auto
 fi
 
-xrandr --output DP-2 --primary
+xrandr --output "$INTERNAL" --primary
