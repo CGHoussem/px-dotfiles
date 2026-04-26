69caca9f196c6d869b4f015b586a8f57bfde6546
Houssem Ben Mabrouk <cjhoussem@gmail.com>
2026-04-26 12:06:35 +0200

    chore(i3): fix lock
    Signed-off-by: Houssem Ben Mabrouk <cjhoussem@gmail.com>

--- i3/.config/i3/scripts/lock.sh
+++ i3/.config/i3/scripts/lock.sh
@@ -3,39 +3,21 @@
 # Lock screen with blur and a beautiful color scheme
 
 # Set background
-img="/tmp/i3lock.png"
-scrot "$img"                    # takes a screenshot
-convert "$img" -blur 0x8 "$img" # applies blur
+img="$HOME/Pictures/retro_wp.jpg"
+# ALTERNATIVE: use a blurred screenshot of the current screen as background
+# img="/tmp/screen.png"
+#scrot "$img"
+#magick convert "$img" -blur 0x8 "$img" # applies blur
 
-# Set colors
-inside=#282c34ff
-ring=#61afefaa
 text=#ffffffcc
-wrong=#e06c75ff
-verifying=#98c379ff
-
 i3lock \
+    --beep \
+    --tiling \
     --image="$img" \
-    --inside-color=373445ff \
-    --ring-color=ffffffff \
-    --line-color=00000000 \
-    --separator-color=222222ff \
-    --verif-color=ffffffff \
-    --wrong-color=ff5555ff \
-    --time-color=ffffffff \
-    --date-color=ffffffff \
-    --layout-color=ffffffff \
-    --keyhl-color=81a2beff \
-    --bshl-color=ff5555ff \
-    --ringver-color=81a2beff \
-    --ringwrong-color=ff5555ff \
-    --indicator \
-    --radius=120 \
-    --ring-width=10 \
-    --time-str="%H:%M" \
-    --date-str="%A, %d %B" \
     --clock \
-    --blur 5 \
-    --force-clock
+    --time-pos="ix:200" \
+    --date-pos="tx:225" \
+    --time-color="$text" \
+    --date-color="$text"
 
-rm "$img"
+# rm "$img"
